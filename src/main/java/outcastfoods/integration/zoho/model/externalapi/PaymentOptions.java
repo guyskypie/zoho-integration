dc8acd196f1b2586dcf176e6ccaff401d170e437
guy <13394282+guyskypie@users.noreply.github.com>
2022-04-08 22:42:54 +0200

    progress adding PNP EDI invoices in csv

---
+++ src/main/java/outcastfoods/integration/zoho/model/externalapi/PaymentOptions.java
@@ -0,0 +1,10 @@
+package outcastfoods.integration.zoho.model.externalapi;
+
+import java.util.ArrayList;
+
+public class PaymentOptions {
+    public ArrayList<PaymentGateway> payment_gateways;
+
+    public PaymentOptions() {
+    }
+}
---
+++ src/main/java/outcastfoods/integration/zoho/model/externalapi/Tax.java
@@ -0,0 +1,9 @@
+package outcastfoods.integration.zoho.model.externalapi;
+
+public class Tax {
+    public String tax_name;
+    public double tax_amount;
+
+    public Tax() {
+    }
+}
